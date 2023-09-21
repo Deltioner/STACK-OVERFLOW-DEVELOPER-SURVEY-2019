8d0954c3c52c424a58f0ef49454c7c34bc298c7c
Deltioner <144788377+Deltioner@users.noreply.github.com>
2023-09-21 12:21:09 +0100

    Delete Codebase S directory

--- Codebase S/array.py
+++
@@ -1,9 +0,0 @@
-count = 1
-programmeertalen = ["PHP", "C#", "HTML", "JavaScript", "Java", "C++", "C"]
-programmeertalen.append('Python')
-programmeertalen.append('Swift')
-programmeertalen.sort()
-print(programmeertalen)
-for i in programmeertalen:
-    print(count,i)
-    count +=1
